3c51cfb4fa6089bde91ae87d8317ec9997078dc4
LOST_TREE <lzh050214@163.com>
2024-06-28 10:16:29 +0800

    Updated Vec3.rs

---
+++ raytracer/src/Vec3.rs
@@ -0,0 +1,175 @@
+use::std::ops::{Add, AddAssign, Sub, SubAssign, Div, Mul, MulAssign, Neg};
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3{
+    pub fn x(&self) -> f64{
+        self.x;
+    }
+
+    pub fn y(&self) -> f64{
+        self.y;
+    }
+
+    pub fn z(&self) -> f64{
+        self.z;
+    }
+
+    pub fn length_squared(&self) -> f64{
+        self.x * self.x + self.y * self.y + self.z + self.z;
+    }
+
+    pub fn length(&self) -> f64{
+        (self.x * self.x + self.y * self.y + self.z + self.z).sqrt();
+    }
+
+    pub fn Mul_Element(a: Self, b: Self) -> Self{
+        Self {
+            x: a.x * b.x,
+            y: a.y * b.y,
+            z: a.z * b.z,
+        }
+    }
+
+    pub fn cross(a: Self, b: Self) -> Self{
+        Self {
+            x: a.y * b.z - a.z * b.y,
+            y: a.z * b.x - a.x * b.z,
+            z: a.x * b.y - a.y * b.x,
+        }
+    }
+
+    pub fn unit(&self) -> Self {
+        let length = self.length();
+        if length == 0.0 {
+            panic!("Divided by 0.0!");
+        } else {
+            *self / length
+        }
+    }
+}
+
+impl Neg for Vec3{
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Add for Vec3{
+    type Output = Self;
+
+    fn add(self, other: self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+
+        }
+    }
+}
+
+impl AddAssign<f64> for Vec3 {
+    fn add_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x + other,
+            y: self.y + other,
+            z: self.z + other,
+        };
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Sub<f64> for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: f64) -> Self {
+        Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
+    }
+}
+
+impl SubAssign<f64> for Vec3 {
+    fn sub_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x - other,
+            y: self.y - other,
+            z: self.z - other,
+        };
+    }
+}
+
+/// Vec3::Mul is dot product.
+impl Mul for Vec3 {
+    type Output = f64;
+
+    fn mul(self, other: Self) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        };
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: f64) -> Self {
+        Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
