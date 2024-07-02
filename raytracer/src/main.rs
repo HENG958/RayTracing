1fbdb9d545556f50864e74e55ffa27aa12417520
LOST_TREE <lzh050214@163.com>
2024-07-02 14:11:40 +0800

    wide-angle view update

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -10,55 +10,73 @@ pub mod ray;
 pub mod sphere;
 pub mod vec3;
 use camera::Camera;
-use material::Dielectric;
-use ray::Point3;
+use color::Color;
+use material::{Dielectric, Lambertian};
 use std::sync::Arc;
+use vec3::Point3;
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image18.jpg");
+    let path = std::path::Path::new("output/book1/image19.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
-    let material_ground = Arc::new(material::Lambertian::new(&color::Color::new(0.8, 0.8, 0.0)));
-    let material_center = Arc::new(material::Lambertian::new(&color::Color::new(0.1, 0.2, 0.5)));
+    //let material_ground = Arc::new(material::Lambertian::new(&color::Color::new(0.8, 0.8, 0.0)));
+    //let material_center = Arc::new(material::Lambertian::new(&color::Color::new(0.1, 0.2, 0.5)));
     //let material_left = Arc::new(material::Metal::new(&color::Color::new(0.8, 0.8, 0.8), 0.3));
 
-    let material_left = Arc::new(Dielectric::new(1.50));
-    let material_bubble = Arc::new(Dielectric::new(1.00 / 1.50));
-    let material_right = Arc::new(material::Metal::new(&color::Color::new(0.8, 0.6, 0.2), 1.0));
+    let _material_left = Arc::new(Dielectric::new(1.50));
+    //let material_bubble = Arc::new(Dielectric::new(1.00 / 1.50));
+    let _material_right = Arc::new(material::Metal::new(&color::Color::new(0.8, 0.6, 0.2), 1.0));
+
+    let r = f64::cos(std::f64::consts::PI / 4.0);
+
+    let material_left = Arc::new(Lambertian::new(&Color::new(0.0, 0.0, 1.0)));
+    let material_right = Arc::new(Lambertian::new(&Color::new(1.0, 0.0, 0.0)));
 
     let mut world = hittable_list::HittableList::new();
-    world.add(Arc::new(sphere::Sphere::new(
-        &Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
+    // world.add(Arc::new(sphere::Sphere::new(
+    //     &Point3::new(0.0, -100.5, -1.0),
+    //     100.0,
+    //     material_ground,
+    // )));
+
+    // world.add(Arc::new(sphere::Sphere::new(
+    //     &Point3::new(0.0, 0.0, -1.2),
+    //     0.5,
+    //     material_center,
+    // )));
+
+    // world.add(Arc::new(sphere::Sphere::new(
+    //     &Point3::new(-1.0, 0.0, -1.0),
+    //     0.5,
+    //     material_left,
+    // )));
+
+    // world.add(Arc::new(sphere::Sphere::new(
+    //     &Point3::new(-1.0, 0.0, -1.0),
+    //     0.4,
+    //     material_bubble,
+    // )));
+
+    // world.add(Arc::new(sphere::Sphere::new(
+    //     &Point3::new(1.0, 0.0, -1.0),
+    //     0.5,
+    //     material_right,
+    // )));
 
     world.add(Arc::new(sphere::Sphere::new(
-        &Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
-    )));
-
-    world.add(Arc::new(sphere::Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.5,
+        &Point3::new(-r, 0.0, -1.0),
+        r,
         material_left,
     )));
 
     world.add(Arc::new(sphere::Sphere::new(
-        &Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-
-    world.add(Arc::new(sphere::Sphere::new(
-        &Point3::new(1.0, 0.0, -1.0),
-        0.5,
+        &Point3::new(r, 0.0, -1.0),
+        r,
         material_right,
     )));
 
-    let mut camera = Camera::new(16.0 / 9.0, 400, 100, 100, 50, 1.0, 2.0);
+    let mut camera = Camera::new(16.0 / 9.0, 400, 100, 100, 50, 1.0, 90.0);
     camera.render(world);
 
     println!(
